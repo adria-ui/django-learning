cbdf77cd48c59924d1cdcf7a0fff408eb5855522
adria-ui <adrian@localhost.localdomain>
2022-12-23 13:28:35 +0300

    Django-learning

--- exercise/pamlo/urls.py
+++ exercise/pamlo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
    path('myfirstpage',views.myfirstpage,name='myfirstpage'),
-   path('mysecondpage',views.mysecondpage,name='mysecondpage')
+   path('mysecondpage',views.mysecondpage,name='mysecondpage'),
+   path('mythirdpage',views.mythirdpage,name='mythirdpage')
 ]
--- exercise/pamlo/views.py
+++ exercise/pamlo/views.py
@@ -8,4 +8,20 @@ def myfirstpage(request):
     return render(request,'index.html')
     
 def mysecondpage(request):
-    return render(request,'second.html')    
+    return render(request,'second.html')
+
+def mythirdpage(request):
+    var ="hello world"
+    brief = "Am adrian malcolm"
+    fruits =["pinaplle","mango","Strawberry"]
+    num1, num2 = 3 , 5 
+    ans = num1 > num2
+    mydictionary = {
+        "var" : var,
+        "msg" : brief,
+        "myfruits" : fruits,
+        "num1" : num1,
+        "num2" : num2,
+        "ans" : ans
+    }
+    return render(request,'third.html',context=mydictionary)
