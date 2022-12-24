8016731f8cd400864c4973232dd189f17ca9daa5
Adrian Malcom <98208724+adria-ui@users.noreply.github.com>
2022-12-24 08:12:01 +0000

    Delete Django directory

--- Django/pamlo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-   path('myfirstpage',views.myfirstpage,name='myfirstpage'),
-   path('mysecondpage',views.mysecondpage,name='mysecondpage'),
-   path('mythirdpage',views.mythirdpage,name='mythirdpage'),
-   path('myimages',views.myimages,name='myimages')
-]
